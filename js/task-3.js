229c28369b31c6080fd17d4b55b42b3f5cf49ae2
defaultnastia <anastayaaa@gmail.com>
2024-05-10 11:42:43 +0200

    home-task done

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const nameInputEl = document.querySelector('#name-input');
-const nameOutputEl = document.querySelector('#name-output');
-function onUsernameInput(event) {
-  nameOutputEl.textContent =
-    event.currentTarget.value.trim() !== ''
-      ? event.currentTarget.value.trim()
-      : 'Anonymous';
-}
-nameInputEl.addEventListener('input', onUsernameInput);
